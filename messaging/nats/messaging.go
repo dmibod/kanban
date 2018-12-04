72d99ed34ebb55c71edb5eef1fbbc746ed61b515
dmibod <Qwerty456>
2018-12-04 09:48:24 +0200

    add client impl stub

--- messaging/nats/messaging.go
+++ messaging/nats/messaging.go
@@ -1,1 +1,14 @@
 package nats
+
+type Client struct{
+}
+
+func (c *Client) Send(subj string, msg []byte){
+}
+
+func (c *Client) Receive(group string, subj string, msg []byte){
+}
+
+func New() *Client {
+  return &Client{}
+}
