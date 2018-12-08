f38f312b8092c0e60212e5d344c4fb9f4f3d87e6
dmibod <Qwerty456>
2018-12-08 19:00:09 +0200

    introduce db api module

---
+++ tools/db/api.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+)
+
+type VisitFn func(interface{})
+
+type Repository interface {
+	
+	Create(interface{}) (string, error)
+	
+	FindById(string) (interface{}, error)
+	
+	Find(interface{}, VisitFn) error
+
+	Count(interface{}) (int, error)
+	
+	Update(interface{}) error
+	
+	Remove(string) error
+}
