5179fce5d4da827cf096dae798a6a2123e9b8b87
dmibod <Qwerty456>
2018-12-14 14:53:45 +0200

    mux: get port from environment variable or default

--- tools/mux/http/impl.go
+++ tools/mux/http/impl.go
@@ -1,23 +1,33 @@
 package http
 
-import(
+import (
 	"github.com/dmibod/kanban/tools/mux"
 
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	defaultPort   = 3000
+	muxPortEnvVar = "MUX_PORT"
 )
 
 var _ mux.Mux = (*Mux)(nil)
 
 // Mux defines mux instance
-type Mux struct{
+type Mux struct {
 	port int
 }
 
 // New - creates mux
 func New(opts ...Option) *Mux {
 	var options Options
+
+	options.Port = GetPortOrDefault(defaultPort)
+
 	for _, o := range opts {
 		o(&options)
 	}
@@ -28,12 +38,23 @@ func New(opts ...Option) *Mux {
 }
 
 // Start - starts mux
-func (m *Mux)Start(){
+func (m *Mux) Start() {
 	log.Printf("Starting mux at port %v...\n", m.port)
 	http.ListenAndServe(fmt.Sprintf(":%v", m.port), nil)
 }
 
 // Handle - attaches url handler to mux
-func (m *Mux)Handle(pattern string, handler http.Handler){
-  http.Handle(pattern, handler)
+func (m *Mux) Handle(pattern string, handler http.Handler) {
+	http.Handle(pattern, handler)
+}
+
+// GetPortOrDefault - gets port from environment variable or fallbacks to default one
+func GetPortOrDefault(defPort int) int {
+	env := os.Getenv(muxPortEnvVar)
+
+	if port, err := strconv.Atoi(env); err != nil {
+		return defPort
+	} else {
+		return port
+	}
 }
