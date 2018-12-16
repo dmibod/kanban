02ecf560778a59785cfc6380eae47d5ebf4d6bda
dmibod <Qwerty456>
2018-12-16 19:02:23 +0200

    mux: add helper methods

--- tools/mux/api.go
+++ tools/mux/api.go
@@ -1,11 +1,24 @@
 package mux
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+// Mux abstracts mux
 type Mux interface {
 	All(string, http.Handler)
 	Get(string, http.Handler)
 	Post(string, http.Handler)
+}
+
+// Json - builds json response
+func Json(w http.ResponseWriter, payload interface{}) {
+	enc := json.NewEncoder(w)
+	enc.Encode(payload)
+}
+
+// Error - builds error response
+func Error(w http.ResponseWriter, code int) {
+	http.Error(w, http.StatusText(code), code)
 }
