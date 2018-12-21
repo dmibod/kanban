74e45216604e726af75ed504b3abbf5313ceacaf
dmibod <Qwerty456>
2018-12-22 00:15:13 +0200

    refactor

--- shared/services/card_service.go
+++ shared/services/card_service.go
@@ -10,6 +10,25 @@ import (
 	"github.com/dmibod/kanban/shared/tools/logger"
 )
 
+// CardPayload represents card fields without id
+type CardPayload struct {
+	Name string
+}
+
+// CardModel represents card at service layer
+type CardModel struct {
+	ID   kernel.Id
+	Name string
+}
+
+// CardService interface
+type CardService interface {
+	// CreateCard creates new card
+	CreateCard(*CardPayload) (kernel.Id, error)
+	// GetCardByID reads card from db by its id
+	GetCardByID(kernel.Id) (*CardModel, error)
+}
+
 type cardService struct {
 	ctx     context.Context
 	logger  logger.Logger
