a886f995777b75b454926eebf88cfce3adc2e238
dmibod <Qwerty456>
2018-12-24 22:26:34 +0200

    fix

--- shared/message/message_service.go
+++ shared/message/message_service.go
@@ -1,10 +1,12 @@
 package message
 
 import (
+	"time"
 	"github.com/dmibod/kanban/shared/tools/circuit"
 	"github.com/dmibod/kanban/shared/tools/circuit/hystrix"
 	"github.com/dmibod/kanban/shared/tools/logger"
 	"github.com/dmibod/kanban/shared/tools/msg/nats"
+	natz "github.com/nats-io/go-nats"
 )
 
 type serviceWithCircuitBreaker struct {
@@ -16,8 +18,16 @@ type serviceWithCircuitBreaker struct {
 
 // CreateService creates database service with circuit breaker
 func CreateService(l logger.Logger) nats.OperationExecutor {
+	e := nats.CreateExecutor(
+		nats.WithLogger(l), 
+		nats.WithReconnectDelay(time.Second),
+		nats.WithName("KANBAN"),
+		nats.WithReconnectHandler(func(c *natz.Conn){ l.Debugln("reconnect...") }),
+		nats.WithDisconnectHandler(func(c *natz.Conn){ l.Debugln("disconnect...") }),
+		nats.WithCloseHandler(func(c *natz.Conn){ l.Debugln("close...") })) 
+
 	return &serviceWithCircuitBreaker{
-		executor: nats.CreateExecutor(nats.WithLogger(l)),
+		executor: e,
 		breaker:  hystrix.New(hystrix.WithLogger(l), hystrix.WithName("NATS"), hystrix.WithTimeout(100)),
 	}
 }
