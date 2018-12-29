a2db7c1a4557d77c95a28b430a4f9660e79b03ca
dmibod <Qwerty456>
2018-12-29 21:53:59 +0200

    update

--- cmd/shared/bus.go
+++ cmd/shared/bus.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"context"
 
-	buz "github.com/dmibod/kanban/shared/bus"
+	"github.com/dmibod/kanban/shared/message"
 	"github.com/dmibod/kanban/shared/tools/bus"
 	"github.com/dmibod/kanban/shared/tools/bus/nats"
 	"github.com/dmibod/kanban/shared/tools/logger"
@@ -29,7 +29,7 @@ func StartBus(ctx context.Context, c string, l logger.Logger) {
 		nats.WithName(c),
 		nats.WithLogger(l))
 
-	if err := bus.ConnectAndServe(ctx, conn, buz.CreateTransport(conn, l)); err != nil {
+	if err := bus.ConnectAndServe(ctx, conn, message.CreateTransport(conn, l)); err != nil {
 		l.Errorf("Mux err: %s\n", err.Error()) // panic if there is an error
 		panic(err)
 	}
