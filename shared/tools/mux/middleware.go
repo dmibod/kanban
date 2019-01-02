9d822760649d35d95df5b0ecbb815157d32088b4
dmibod <Qwerty456>
2019-01-02 03:25:41 +0200

    fix

--- shared/tools/mux/middleware.go
+++ shared/tools/mux/middleware.go
@@ -6,15 +6,17 @@ import (
 	"github.com/dmibod/kanban/shared/tools/db/mongo"
 )
 
-// SessionMiddleWare provides mongo session
-type SessionMiddleWare struct {
-	sp mongo.SessionProvider
+// CreateSessionProvider middleware
+func CreateSessionProvider(sp mongo.SessionProvider) func(http.Handler) http.Handler {
+	fn := func(next http.Handler) http.Handler {
+		return sessionProvider(sp, next)
+	}
+	return fn
 }
 
-// SessionProvider puts session in context and calls next handler
-func (m *SessionMiddleWare) SessionProvider(next http.Handler) http.Handler {
+func sessionProvider(sp mongo.SessionProvider, next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		ctx := m.sp.WithSession(r.Context())
+		ctx := sp.WithSession(r.Context())
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 	return http.HandlerFunc(fn)
